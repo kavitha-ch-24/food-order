32b44e157f9eb339f4b92aec9e18396bdeaff4ad
kavich <kavitha.ch@ittstar.com>
2024-09-19 19:25:09 +0530

    new

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,5 +10,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor() { 
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get myfc() {
+    return this.loginForm.controls;
+  }
 
 }
--- src/app/components/user/register-user/register-user.component.ts
+++ src/app/components/user/register-user/register-user.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
 export class RegisterUserComponent {
+  registerForm: FormGroup;
+  dataVerify: boolean = false;
 
+  constructor() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
+      gender: new FormControl('', Validators.required),
+      dob: new FormControl('', Validators.required)
+    })
+  }
+
+  get myfc() {
+    return this.registerForm.controls;
+  }
+
+  createAccount() {
+    this.dataVerify = true;
+    console.log(this.registerForm.controls);
+    console.log(this.registerForm.value);
+  }
 }
