370882e711c5b45c43b3c4cd1af2ac051cf1dae5
kavich <kavitha.ch@ittstar.com>
2024-09-19 16:32:29 +0530

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterUserComponent } from './components/user/register-user/register-user.component';
 
 export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'register', component: RegisterUserComponent },
     { path: '', component: LoginComponent },
 ];
--- src/app/components/user/register-user/register-user.component.ts
+++ src/app/components/user/register-user/register-user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
